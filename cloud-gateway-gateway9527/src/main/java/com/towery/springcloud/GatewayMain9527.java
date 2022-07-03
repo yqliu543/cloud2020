0943e0cd53cf7b8654118daf9a79897c8e03bcf2
yqliu543 <80449290+yqliu543@users.noreply.github.com>
2022-07-03 15:00:35 +0800

    commit 9527,time:2022/7/3

--- cloud-gateway-gateway9527/src/main/java/com/towery/springcloud/GatewayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/towery/springcloud/GatewayMain9527.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayMain9527 {
+    
     public static void main(String[] args) {
         SpringApplication.run(GatewayMain9527.class,args);
     }
